6efeea972e7dbf97b0fde5e51704905b77afbb00
Alberto Ruffo <alberto@prismaphp.org>
2013-06-12 13:03:00 +0200

    small corrections

--- 404.php
+++ 404.php
@@ -1,72 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# fedom_it: http://ogp.me/ns/fb/fedom_it#">
-        <meta charset="utf-8">
-        <title>404 <?php echo Config::APP; ?></title>
-        <meta property="fb:app_id" content="170768719638822" /> 
-        <meta property="og:type"   content="fedom_it:product" /> 
-        <meta property="og:url"    content="http://www.fedom.it/product/201/bluetimes-bt-3550hd-network-hdd-media-player-full-hd-wifi-dvb-t-recorder-hd-mkv-chip-realtek-ex-bt-3549-fed0166" /> 
-        <meta property="og:title"  content="Bluetimes hdd media player" /> 
-        <meta property="og:image"  content="http://www.fedom.it/foto/bt3550hd.jpg" /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="Alberto Ruffo">
-
-        <!-- Le styles -->
-        <link href="<?php echo Config::INDEX_URL; ?>/public/css/bootstrap.css" rel="stylesheet">
-        <link href="<?php echo Config::INDEX_URL; ?>/public/css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="<?php echo Config::INDEX_URL; ?>/public/css/personalize.css" rel="stylesheet">
-        <link href="<?php echo Config::INDEX_URL; ?>/public/css/mobile.css" rel="stylesheet">
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-          <script src="<?php echo Config::INDEX_URL; ?>/public/js/html5shiv.js"></script>
-        <![endif]-->
-
-        <!-- Fav and touch icons -->
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo Config::INDEX_URL; ?>/public/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Config::INDEX_URL; ?>/public/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Config::INDEX_URL; ?>/public/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="<?php echo Config::INDEX_URL; ?>/public/ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="<?php echo Config::INDEX_URL; ?>/public/ico/favicon.png">
-    </head>
-    <body>
-        
-        <div class="container">
-
-      <div class="masthead">
-        <h3 class="muted"><img style="width:120px;" src="<?php echo Config::INDEX_URL; ?>/public/img/logo.png" alt="Fedom.it"/></h3>
-
-        
-        <!-- /.navbar -->
-      </div>
-
-      <!-- Jumbotron -->
-      
-      <div class="row-fluid">
-      <h1>Pagina non trovata</h1>
-      <p><?php echo $message; ?> -> <a href="<?php echo Config::INDEX_URL; ?>">Home</a></p>
-      </div>
-      
+<head>
+    <meta charset="utf-8">
+    <title>404 <?php echo Config::APP; ?></title>
+</head>
+<body>
 
 
- 
+<div class="row-fluid">
+    <h1>Page not found</h1>
 
-      <hr>
-
-      <div class="footer">
-        <p>&copy; fedom.it 2013</p>
-      </div>
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="<?php echo Config::INDEX_URL; ?>/public/js/bootstrap.js"></script>
-     
+    <p><?php echo $message; ?> -> <a href="<?php echo Config::INDEX_URL; ?>">Home</a></p>
+</div>
 
 
-
-    </body>
+</body>
 </html>
