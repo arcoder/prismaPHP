b111cc29c6083ab4632a785406b44218c9a46124
Alberto Ruffo <alberto@prismaphp.org>
2013-06-28 17:20:50 +0200

    Foundation 4 added

--- config/Config.php
+++ config/Config.php
@@ -8,165 +8,166 @@
 date_default_timezone_set('Europe/Rome');
 setlocale(LC_ALL, 'it_IT');
 
-class Config {
+class Config
+{
 
     /**
      * SITE
-     * 
+     *
      * (default value: "http://localhost:8888/framework)
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
-    const INDEX_URL = "http://localhost:8888/prismaPHP";
-    
+    const INDEX_URL = "http://localhost:8888/prismaPHP-master";
+
 
     const DOCS = "http://phprisma.org/docs.html";
-    
+
     /**
      * APP
-     * 
+     *
      * (default value: "prismaPHP")
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const APP = "prismaPHP";
-    
+
     /**
      * SMTPhost
-     * 
+     *
      * (default value: "smtp.site.it")
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const SMTP_HOST = "mail.site.it";
-   
-     
+
+
     /**
      * SMTPusername
-     * 
+     *
      * (default value: "email@site.it")
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const SMTP_USERNAME = "email@site.it";
-    
+
     /**
      * SMTPpassword
-     * 
+     *
      * (default value: "password")
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const SMTP_PASSWORD = "password";
-    
+
     /**
      * SMTPport
-     * 
+     *
      * (default value: '25')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const SMTP_PORT = '25';
-    
+
     /**
      * adapter
-     * 
+     *
      * (default value: 'mysql')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const DB_ADAPTER = 'mysql'; //mysql, postgresql, sqlite, no
-    
+
     /**
      * dbname
-     * 
+     *
      * (default value: 'prova')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const DB_DATABASE = 'framework';
-    
+
     /**
      * dbhost
-     * 
+     *
      * (default value: 'localhost')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const DB_HOST = 'localhost'; //host or sqlite db file
-    
+
     /**
      * dbuser
-     * 
+     *
      * (default value: 'root')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const DB_USER = 'root';
-    
+
     /**
      * dbpassword
-     * 
+     *
      * (default value: 'root')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const DB_PASSWORD = 'root';
-    
+
     /**
      * default_language
-     * 
+     *
      * (default value: 'it')
-     * 
+     *
      * @var string
      * @access public
      * @static
      */
     const LANG_DEFAULT = 'it-it';
-    
+
     /**
      * http_language_enabled
-     * 
+     *
      * (default value: false)
-     * 
+     *
      * @var bool
      * @access public
      * @static
      */
     const LANG_MULTI_LANGUAGE = false;
-    
 
-	 /**
-	  * DEVELOPMENT_ENV
-	  * 
-	  * (default value: true)
-	  * 
-	  * @var bool
-	  * @access public
-	  * @static
-	  */
-	 const DEVELOPMENT_ENV = true;
+
+    /**
+     * DEVELOPMENT_ENV
+     *
+     * (default value: true)
+     *
+     * @var bool
+     * @access public
+     * @static
+     */
+    const DEVELOPMENT_ENV = true;
 
 }
--- view/layouts/hello.html.php
+++ view/layouts/hello.html.php
@@ -1,54 +1,102 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width"/>
 
-        <title>prismaPHP Framework page</title>
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<!-- YOU CAN USE Bootstrap OR only normalize.css
-    	<link href="<?php echo Config::INDEX_URL; ?>/public/css/bootstrap.min.css" rel="stylesheet" media="screen">   
-    	-->     
-    	<link href="<?php echo Config::INDEX_URL; ?>/public/css/normalize.css" media="screen" rel="stylesheet" type="text/css" />    			
-    	<link href="<?php echo Config::INDEX_URL; ?>/public/css/main.css" media="screen" rel="stylesheet" type="text/css" />        
-    </head>
-    <body>
-        <div id="intestazione">
-            <div id="logo"><a href="<?php echo Config::INDEX_URL; ?>"><span>prismaPHP Framework</span></a></div>
-            <div id="urls">
-                <ul>
-                    <li><a href="<?php echo Config::DOCS; ?>/contacts">contacts</a></li>
-                    <li><a href="https://github.com/arcoder/prismaPHP">github</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id="manifest">
-            <div id="what"><a href="<?php echo Config::DOCS; ?>"><h1>prismaPHP Framewodsrk</h1></a></div>
-            <div id="download"><a href="<?php echo Config::DOCS; ?>"><h2>prismaPHP Framewodsrk</h2></a></div>
-            <div id="menu">
-                <ul class="menuNavigazione">
-                    <li><a href="<?php echo Config::DOCS; ?>/docs/page/installation?id=1">install</a></li>
-                    <li class="voceCorrente"><a href="<?php echo Config::DOCS; ?>/docs/index">documentation</a></li>
-                    <li><a href="#">community</a></li>
-                    <li><a href="#">screencasts</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id="corpo">
-            <div id="corpo-sub">
-                <div id="colonna-1">
-                	<?php Success::get('general'); ?>
-                	<?php Error::get('general'); ?>
-                    <?php $this->render(self::$routes['controller'], self::$routes['action']); ?>
-                    <?php $this->partial('layouts/tpl/twitter'); ?>
-                    <?php $this->partial('layouts/tpl/about'); ?>
-                </div>
-
-            </div>
-        </div>
-        <div id="pie-di-pagina"><p>prismaPHP <?php echo date('Y', time()); ?></p></div>
-    	<script src="http://code.jquery.com/jquery.js"></script>
-    	<!-- YOU CAN USE BOOTSTRAP
-    	<script src="<?php echo Config::INDEX_URL; ?>/public/js/bootstrap.min.js"></script>
+    <title>prismaPHP Framework page</title>
+    <!-- YOU CAN USE Foundation 4 OR only normalize.css
+    	<link href="<?php echo Config::INDEX_URL; ?>/public/css/foundation.css" rel="stylesheet" media="screen">   
     	-->
-    </body>
+    <link href="<?php echo Config::INDEX_URL; ?>/public/css/normalize.css" media="screen" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?php echo Config::INDEX_URL; ?>/public/css/main.css" media="screen" rel="stylesheet" type="text/css"/>
+
+    <script src="<?php echo Config::INDEX_URL; ?>/public/js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+<div id="intestazione">
+    <div id="logo"><a href="<?php echo Config::INDEX_URL; ?>"><span>prismaPHP Framework</span></a></div>
+    <div id="urls">
+        <ul>
+            <li><a href="<?php echo Config::DOCS; ?>/contacts">contacts</a></li>
+            <li><a href="https://github.com/arcoder/prismaPHP">github</a></li>
+        </ul>
+    </div>
+</div>
+<div id="manifest">
+    <div id="what"><a href="<?php echo Config::DOCS; ?>"><h1>prismaPHP Framewodsrk</h1></a></div>
+    <div id="download"><a href="<?php echo Config::DOCS; ?>"><h2>prismaPHP Framewodsrk</h2></a></div>
+    <div id="menu">
+        <ul class="menuNavigazione">
+            <li><a href="<?php echo Config::DOCS; ?>/docs/page/installation?id=1">install</a></li>
+            <li class="voceCorrente"><a href="<?php echo Config::DOCS; ?>/docs/index">documentation</a></li>
+            <li><a href="#">community</a></li>
+            <li><a href="#">screencasts</a></li>
+        </ul>
+    </div>
+</div>
+<div id="corpo">
+    <div id="corpo-sub">
+        <div id="colonna-1">
+            <?php Success::get('general'); ?>
+            <?php Error::get('general'); ?>
+            <?php $this->render(self::$routes['controller'], self::$routes['action']); ?>
+            <?php $this->partial('layouts/tpl/twitter'); ?>
+            <?php $this->partial('layouts/tpl/about'); ?>
+        </div>
+
+    </div>
+</div>
+<div id="pie-di-pagina"><p>prismaPHP <?php echo date('Y', time()); ?></p></div>
+<script src="http://code.jquery.com/jquery.js"></script>
+<!-- YOU CAN USE FOUNDATION
+<script>
+document.write('<script src=' +
+('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+'.js><\/script>')
+</script>
+
+<script src="js/foundation.min.js"></script>
+
+<script src="js/foundation/foundation.js"></script>
+
+<script src="js/foundation/foundation.alerts.js"></script>
+
+<script src="js/foundation/foundation.clearing.js"></script>
+
+<script src="js/foundation/foundation.cookie.js"></script>
+
+<script src="js/foundation/foundation.dropdown.js"></script>
+
+<script src="js/foundation/foundation.forms.js"></script>
+
+<script src="js/foundation/foundation.joyride.js"></script>
+
+<script src="js/foundation/foundation.magellan.js"></script>
+
+<script src="js/foundation/foundation.orbit.js"></script>
+
+<script src="js/foundation/foundation.reveal.js"></script>
+
+<script src="js/foundation/foundation.section.js"></script>
+
+<script src="js/foundation/foundation.tooltips.js"></script>
+
+<script src="js/foundation/foundation.topbar.js"></script>
+
+<script src="js/foundation/foundation.interchange.js"></script>
+
+<script src="js/foundation/foundation.placeholder.js"></script>
+
+
+
+<script>
+$(document).foundation();
+</script>
+-->
+</body>
 </html>
