f9712db252f5d90782bf51de9f3260d3938a92b1
Alberto Ruffo <alberto@prismaphp.org>
2013-07-26 15:29:42 +0200

    new .htaccess management and public/ rewrite rule

--- pf/rc.php
+++ pf/rc.php
@@ -681,12 +681,9 @@ class URI
     private function __construct($url = null)
     {
         if (is_null($url)) {
-            #if (isset($_SERVER['ORIG_PATH_INFO']))
-            #    $this->path = substr($_SERVER['ORIG_PATH_INFO'], 1);
-            if (isset($_SERVER['PATH_INFO']))
-                $this->path = substr($_SERVER['PATH_INFO'], 1);
-            elseif (isset($_SERVER['REQUEST_URI']))
-                $this->path = substr($_SERVER['REQUEST_URI'], 1); else
+            if (isset($_GET['__PHURL__'])) {
+                $this->path = $_GET['__PHURL__'];
+            } else
                 $this->path = '';
         } else {
             $this->path = $url;
--- public/index.php
+++ public/index.php
@@ -4,52 +4,49 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-
+session_start();
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
+define('ROOT', dirname(dirname(__FILE__)));
 
-function autoload($classname) {
+function autoload($classname)
+{
     if (is_readable(ROOT . DS . 'format' . DS . $classname . '.php'))
-        require_once( "format" . DS . "$classname.php" );
+        require_once(ROOT . DS . "format" . DS . $classname . '.php');
     if (is_readable(ROOT . DS . 'model' . DS . $classname . '.php'))
-        require_once( "model" . DS . "$classname.php" );
+        require_once(ROOT . DS . "model" . DS . $classname . '.php');
     if (is_readable(ROOT . DS . 'lib' . DS . $classname . '.php'))
-        require_once( 'lib' . DS . "$classname.php" );
+        require_once(ROOT . DS . 'lib' . DS . $classname . '.php');
 }
 
 spl_autoload_register('autoload');
 
-require_once 'config/Config.php';
+require_once ROOT . DS . 'config/Config.php';
 require_once ROOT . DS . 'pf' . DS . 'rb.php';
 require_once ROOT . DS . 'pf' . DS . 'rc.php';
-require_once 'config/AppRoute.php';
+require_once ROOT . DS . 'config/AppRoute.php';
 DB::configure();
-
 
 
 ############### Helper ###############
 require_once ROOT . DS . 'helper' . DS . 'H.php';
 ############### ApplicationController ###############
-require_once 'controller' . DS . 'ApplicationController.php';
-
+require_once ROOT . DS . 'controller' . DS . 'ApplicationController.php';
 
 
 ############### VENDOR LIBS ################
 
 ## Swiftmailer
-require_once ROOT . DS . 'vendor'. DS .'swiftmailer5.0.0'. DS .'swift_required.php';
+require_once ROOT . DS . 'vendor' . DS . 'swiftmailer5.0.0' . DS . 'swift_required.php';
 
-function swiftmailer_configurator() {
+function swiftmailer_configurator()
+{
     // configure Swift Mailer
     Swift_Preferences::getInstance()->setCharset('utf-8');
 }
 
 Swift::init('swiftmailer_configurator');
 Mail::setTransport();
-
-
-
 
 
 /*
@@ -63,7 +60,6 @@ register_shutdown_function('session_write_close');
 session_start();
 */
 
-session_start();
 
 try {
     $app = AppRoute::getInstance(URI::getInstance());
@@ -88,7 +84,7 @@ try {
 } catch (ControllerException $e) {
     $e->__toString();
 } catch (FormatHandlerException $e) {
-    $e->__toString_No404();
-    ;
-} 
-DB::close(); 
+    $e->__toString_No404();;
+}
+
+DB::close();
